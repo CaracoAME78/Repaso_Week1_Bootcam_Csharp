886cfc7c14b1aaa78c53eb8b977a1bb18fd68986
Carlos Ramos <carlosrc78@gmail.com>
2025-04-09 21:36:30 -0500

    Challenge Modulo 1

---
+++ RepasoModulo1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace RepasoModulo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fila1 = 1;
+            int columna1;
+
+            while (fila1 <= 5)
+            {
+                columna1 = 1;
+                while (columna1 <= fila1)
+                {
+                    Console.Write(columna1);
+                    columna1++;
+                }
+                Console.WriteLine();
+                fila1++;
+            }
+
+        }
+    }
+}
